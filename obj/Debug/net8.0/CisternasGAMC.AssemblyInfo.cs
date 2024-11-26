c45e8855000bccf2b564fd696f13285054e5f84b
DavidAcha76 <agd0033427@est.univalle.edu>
2024-11-26 15:18:46 -0400

    Fin detalles QA

--- obj/Debug/net8.0/CisternasGAMC.AssemblyInfo.cs
+++ obj/Debug/net8.0/CisternasGAMC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CisternasGAMC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86114e9fe0116077f763e6c56fc5ea02030e1382")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05a5ae7f1b3d465436f7a0b918b3129253938b06")]
 [assembly: System.Reflection.AssemblyProductAttribute("CisternasGAMC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CisternasGAMC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
